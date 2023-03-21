a157f5773ba815969ae07358faee0a264ead2e0c
khalid fifel <fifle.khalid99@gmail.com>
2023-03-21 09:19:30 +0000

    add the relationships to Book and User model

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -8,4 +8,24 @@ use Illuminate\Database\Eloquent\Model;
 class Book extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'author',
+        'isbn',
+        'number_pages',
+        'location',
+        'status',
+        'content',
+    ];
+
+    public function genre()
+    {
+        return $this->belongsTo(Genre::class);
+    }
+
+    public function collection()
+    {
+        return $this->belongsTo(Collection::class);
+    }
 }
